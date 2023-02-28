5b64575be1b7fd6db8324f8e11d55a5919fcc43d
Tommaso Ferrario <91728535+tommi00@users.noreply.github.com>
2023-02-28 11:32:55 +0100

    Add files via upload

---
+++ arduino_relay_udp_tommi.ino
@@ -0,0 +1,95 @@
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+
+const int RELAY_PIN  = 9;                             // RELAY PIN
+const char relayOn[] = "on";
+const char relayOff[] = "off";
+
+int relayState = HIGH;                                 // STATO INIZIALE RELAY
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };  // mac address (keep this one when unknown)
+byte ip[] = { 192, 168, 2, 50 };                      // ip address
+byte gateway[] = { 192, 168, 2, 1 };                  // gateway rete
+byte subnet[] = { 255, 255, 255, 0 };                 // subnet mask
+
+unsigned int localPort = 9001;      // local port to listen on
+
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];  // buffer to hold incoming packet,
+char ReplyBuffer[] = "acknowledged";        // a string to send back
+
+// An EthernetUDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+
+void setup() {
+  Ethernet.begin(mac, ip);
+
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+  // start UDP
+  Udp.begin(localPort);
+}
+
+void loop() {
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+  
+  if (packetSize) {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remote = Udp.remoteIP();
+    for (int i=0; i < 4; i++) {
+      Serial.print(remote[i], DEC);
+      if (i < 3) {
+        Serial.print(".");
+      }
+    }
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
+
+    // read the packet into packetBufffer
+    Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    //Serial.println("expected command:");
+    //Serial.println(relayOn);
+    Serial.println("Contents:");
+    Serial.println(packetBuffer);
+    //Serial.println("comp:");
+    //Serial.println(strcmp(relayOn, packetBuffer));
+
+    if(!strcmp(relayOn, packetBuffer) ){
+      Serial.println("command ON received");
+      relayState = LOW;
+      digitalWrite(RELAY_PIN, relayState);
+      Serial.println("relay turned on");
+    }else if (!strcmp(relayOff, packetBuffer) ){
+      Serial.println("command OFF received");
+      relayState = HIGH;
+      digitalWrite(RELAY_PIN, relayState);
+      Serial.println("relay turned off");
+    }
+
+    // send a reply to the IP address and port that sent us the packet we received
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(ReplyBuffer);
+    Udp.endPacket();
+  }
+  delay(10);
+}
